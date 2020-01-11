90ffc5def2e3b4d1cfa69b47e7382f32e4a6c170
Alexander Garcia <alexg97343@gmail.com>
2020-01-11 17:22:53 -0500

    save and delete needs to be fixed

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,9 +1,5 @@
-// ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
+// We are linking our routes to a series of data sources.
 const fs = require("fs");
 
 var notes = require("../db/db.json");
@@ -11,29 +7,29 @@ var notes = require("../db/db.json");
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
-       return res.json(notes);
+       return res.json(notesInfo);
       });
 
     app.post("/api/notes", function(req,res){
         let newNote = req.body;
         console.log(newNote);
         notes.push(newNote);
-        console.log(notes);
+        console.log(notesInfo);
         addId();
-        let save = JSON.stringify(notes);
+        let save = JSON.stringify(notesInfo);
         fs.writeFileSync("./db/db.json",save)
 
         res.redirect('back');
     });
     app.delete("/api/notes/:id", function (req,res) {
         console.log(req.params.id);
-        notes.splice((req.params.id -1),1);
+        notesInfo.splice((req.params.id -1),1);
         fs.writeFileSync("./db/db.json",save);
         res.redirect('back');
     });
 
     function addId() {
-        notes.forEach((element,i) => {
+        notesInfo.forEach((element,i) => {
             element.id = i+1;
         });
     }
---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+const fs = require("fs");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+
+// The below code starts our server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
